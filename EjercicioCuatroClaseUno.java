7cc07a31164a0099c6b07c89fb108a572866608c
Marcelo Daniel <danielmarcelo83@gmail.com>
2019-05-21 17:50:07 -0300

    Update EjercicioCuatroClaseUno.java

--- EjercicioCuatroClaseUno.java
+++ EjercicioCuatroClaseUno.java
@@ -1,3 +1,4 @@
+  
 /*Ingresar el nombre de un articulo con su precio. Imprimir el importe a 
 pagar. Si recibe un descuento del 12porciento se lo abona de contado.
 */
@@ -10,18 +11,15 @@ public static void main(String arg[])
     {
     c = new Console();
     String articulo;
-    float precio;
-    double descuento, importeAPagar;
-    
-    
-    articulo = "viaje";
+    float precio, descuento, importeAPagar;
+
     c.println("Ingrese nombre del articulo: ");
     articulo = c.readString();
     
     c.println("Ingrese precio: ");
     precio = c.readFloat();
     
-    descuento = precio*0.12;
+    descuento = precio*12/100;
     importeAPagar = precio-descuento;
     
     c.println("El importe total con descuento del 12% es de: "+importeAPagar);
