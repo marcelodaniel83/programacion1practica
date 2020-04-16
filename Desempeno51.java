6c27ed66657f857877d3970ed9749194ce8a35f1
Marcelo Daniel <danielmarcelo83@gmail.com>
2020-04-15 21:45:17 -0300

    Add files via upload
    Clase 4

---
+++ Desempeno51.java
@@ -0,0 +1,23 @@
+/*
+Imprimí los números pares en forma
+decreciente desde el 800 hasta el 400
+*/
+
+import hsa.Console;
+class Desempeno51
+{
+ static Console c;
+ public static void main (String arg [])
+ {
+ int i;
+ c = new Console ();
+ i = 800;
+ while (i >= 400)
+ {
+ c.print (i + " ");
+ i = i - 1;
+ }
+ }
+}
+
+
