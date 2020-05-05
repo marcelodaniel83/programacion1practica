241b2338704f1b7ac414f036d9735d9548c32cd3
Marcelo Daniel <danielmarcelo83@gmail.com>
2020-05-05 00:44:37 -0300

    Create imprimirDesc.java

---
+++ refuerzosDeEjercicios/imprimirDesc.java
@@ -0,0 +1,13 @@
+/*
+Mediante la programación en Java, desarrollar un programa que permita
+imprimir de forma descendente números que van del 57 hasta el 21.
+*/
+
+public class Main {
+
+    public static void main(String[] args) {
+       for(int i=57;i>=21; i=i-2){
+           System.out.println(” ” +i);
+       }
+    }  
+}
