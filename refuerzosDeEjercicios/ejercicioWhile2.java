cb22d38d2d201f7ee1ec942ca1c0f1dcb8bc31a8
Marcelo Daniel <danielmarcelo83@gmail.com>
2020-05-07 23:11:05 -0300

    Create ejercicioWhile2.java

---
+++ refuerzosDeEjercicios/ejercicioWhile2.java
@@ -0,0 +1,33 @@
+/*
+Desarrollar un programa que permita cargar n números enteros
+y luego nos informe cuántos valores fueron pares y cuántos impares.
+
+*/
+
+import java.util.Scanner;
+
+public class EstructuraRepetitivaWhile11 {
+    public static void main(String[] ar) {
+        Scanner teclado=new Scanner(System.in);
+        int n,x,valor,pares,impares;
+        x=1;
+        pares=0;
+        impares=0;
+        System.out.print("Cuantos números ingresará:");
+        n=teclado.nextInt();
+        while (x<=n) {
+            System.out.print("Ingrese el valor:");
+            valor=teclado.nextInt();
+            if (valor%2==0) {
+                pares=pares + 1;
+            } else {
+                impares=impares + 1;
+            }
+            x=x + 1;
+        }
+        System.out.print("Cantadad de pares:");
+        System.out.println(pares);
+        System.out.print("Cantidad de impares:");
+        System.out.print(impares);
+    }
+}
